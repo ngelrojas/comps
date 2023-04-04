b65aac8eccb98f62e6478bc0512b73a2b8f28bf6
ngel rojas <aomarrojasp@gmail.com>
2023-04-03 21:19:06 -0300

    add: adding tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <div>
+                <Button >click here</Button>
+            </div>
+
+            <div>
+                <Button >click here1 </Button>
+            </div>
+
+            <div>
+                <Button >click here2</Button>
+            </div>
+
+            <div>
+                <Button >click here3</Button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
