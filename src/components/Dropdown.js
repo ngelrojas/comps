154852d913f89d9ef1b3b0a8ee91aa5363daf12e
ngel rojas <aomarrojasp@gmail.com>
2023-04-26 21:38:55 -0300

    add: adding useRef and put it in dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import {GoChevronDown} from 'react-icons/go';
 import Panel from "./Panel";
 
 export default function Dropdown({options, value, onChange}){
     const [isOpen, setIsOpen] = useState(false);
+    const divEvl = useRef(null);
 
     const handleClick = () => {
         setIsOpen((currentIsOpen) => !currentIsOpen);
@@ -27,8 +28,22 @@ export default function Dropdown({options, value, onChange}){
     //     content = selection.label;
     // }
 
+    useEffect(()=> {
+        const handler = (event) => {
+            if(!divEvl.current) return;
+            if (!divEvl.current.contains(event.target)){
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener('click', handler, true);
+        return () => {
+            document.removeEventListener('click', handler);
+        }
+    }, [])
+    
+
     return (
-        <div className="w-48 relative">
+        <div ref={divEvl} className="w-48 relative">
             <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
                 {value?.label || 'Select...'}
                 <GoChevronDown className="text-lg" />
